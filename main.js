78c7b58cd7ae9afc8ca57b5e661e43fcf5c51eb4
Dimon1UA <92036555+Dimon1UA@users.noreply.github.com>
2022-01-22 16:08:32 +0200

    beginer

---
+++ main.js
@@ -0,0 +1,171 @@
+let message = 'Приветствую тебя, мой друг! Давай порешаем задачки!'
+alert(message)
+
+// Beginer_1
+// let a = +prompt ('Введите сторону квадрата, чтобы узнать периметр квадрата')
+// let b = 4*a
+// alert(b)
+// console.log(b)
+
+// Beginer_2
+// let a = +prompt ('Введите строну квадрата, чтобы узнать его площадь')
+// let b = a**2
+// alert(b)
+// console.log(b)
+
+// Beginer_3
+// let a = +prompt ('Введите первую сторону прямоугольника')
+// let b = +prompt ('Введите вторую сторону прямоугольника')
+// let S = a*b
+// let P = 2*(a+b)
+// alert('Вот его площадь: ' + S)
+// alert('А это периметр: ' + P)
+// console.log(S)
+// console.log(P)
+
+// Beginer_4
+// let d = +prompt ('Введите диаметр окружности, чтобы узнать ее длину')
+// let L = 3.14*d
+// alert(L)
+// console.log(L)
+
+// Beginer_5
+// let a = +prompt ('Введите длину ребра куба, чтобы узнать его объем и площадь его поверхности')
+// let V = a**3
+// let S = 6*(a**2)
+// alert('Вот его объем: ' + V)
+// alert('А это площадь его поверхности: ' + S)
+// console.log(V)
+// console.log(S)
+
+// Beginer_6
+// let a = +prompt ('Введите длину первого ребра куба')
+// let b = +prompt ('Введите длину второго ребра куба')
+// let c = prompt ('Введите длину третьего ребра куба')
+// let V = a*b*c
+// let S = 2*(a*b+b*c+a*c)
+// alert('Вот его объем: ' + V)
+// alert('А это площадь его поверхности: ' + S)
+// console.log(V)
+// console.log(S)
+
+// Beginer_7
+// let R = +prompt ('Укажи радиус окружности')
+// let L = 2*3.14*R
+// let S = 3.14*(R**2)
+// alert('Ты нашел длину окружности: ' + L)
+// alert('И площадь круга: ' + S)
+// console.log(L)
+// console.log(S)
+
+// Beginer_8
+// let a = +prompt ('Введи первое число')
+// let b = +prompt ('Введи второе число')
+// let c = (a+b)/2
+// alert('Ты нашел их среднее арифметическое: ' + c)
+// console.log(c)
+
+// Beginer_9
+// let a = +prompt ('Введите первое неотрицательное число')
+// let b = +prompt ('Введите второе неотрицательное число')
+// let c = (a*b)**0.5
+// alert('Вы нашли их среднее геометрическое: ' + c)
+// console.log(c)
+
+// Beginer_10
+// let a = +prompt ('Введите первое ненулевое число')
+// let b = +prompt ('Введите второе ненулевое число')
+// let c = a+b
+// let d = a-b
+// let e = a*b
+// let f = (a**2)/(b**2)
+// alert('Вот их сумма: ' + c + ' ; это разность: '+ d + ' ; это произведение: '+ e +' ; а это частное их квадратов: ' + f )
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+
+// Beginer_11
+// let a = +prompt ('Введите первое ненулевое число')
+// let b = +prompt ('Введите второе ненулевое число')
+// let c = a+b
+// let d = a-b
+// let e = a*b
+// let f = Math.abs(a)/Math.abs(b)
+// alert('Вот их сумма: ' + c + ' ; это разность: '+ d + ' ; это произведение: '+ e +' ; а это частное их модулей: ' + f )
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+
+// Beginer_12
+// let a = +prompt ('Введите значение первого катета прямоугольного треугольника')
+// let b = +prompt ('Введите значение второго катета прямоугольного треугольника')
+// let c = ((a**2)+(b**2))**0.5
+// let P = a+b+c
+// alert('Вы нашли гепотенузу треугольника: ' + c + ' и его периметр: ' + P)
+// console.log(c)
+// console.log(P) 
+
+// Beginer_13
+// let R1 = +prompt ('Введите радиус первого круга, с центром в точке О')
+// let R2 = +prompt ('Введите радиус второго круга (меньший чем радиус первого круга), с центром в тойже точке О')
+// let S1 = 3.14*(R1**2)
+// let S2 = 3.14*(R2**2)
+// let S3 = S1-S2
+// alert('Вы нашли площадь первого круга: ' + S1 + ' , второго: ' + S2 + ' , а также площадь кольца, внешний радиус которого равен R1, а внутренний радиус равен R2: ' + S3)
+// console.log(S1)
+// console.log(S2)
+// console.log(S3)
+
+// Beginer_14
+// let L = +prompt ('Укажите длину окружности')
+// let R = L/(2*3.14)
+// let S = 3.14*(R**2)
+// alert('Это радиус окружности: ' + R + ' и площадь круга, ограниченого этой окружностью: ' + S)
+// console.log(R)
+// console.log(S)
+
+// Beginer_15
+// let S = +prompt ('Укажите площадь круга')
+// let D = ((4*S)/3.14)**0.5
+// let L = 3.14*D
+// alert('Вы нашли его диаметр: ' + D + ' и длину окружности, ограничивающей этот круг: ' + L)
+// console.log(D)
+// console.log(L)
+
+// Beginer_25
+// let x = +prompt ('Введите значение x')
+// let y = 3*(x**6)-6*(x**2)-7
+// alert(y)
+// console.log(y)
+
+// Beginer_26
+// let x = +prompt ('Введите значение x')
+// let y = (4*(x-3)**6)-(7*(x-3)**3)+2
+// alert(y)
+// console.log(y)
+
+// Beginer_27
+// let A = +prompt ('Ведите число, чтобы возвести его в восьмую степень')
+// let A2 = A**2
+// let A4 = A**4
+// let A8 = A**8
+// alert(`A2 = ${A2}; A4 = ${A4}; A4 = ${A8}`)
+// console.log(A2)
+// console.log(A4)
+// console.log(A8)
+
+// Beginer_28
+// let A = +prompt ('Ведите число, чтобы возвести его в пятнадцатую степень')
+// let A2 = A**2
+// let A3 = A**3
+// let A5 = A**5
+// let A10 = A**10
+// let A15 = A**15
+// alert(`A2 = ${A2}; A3 = ${A3}; A5 = ${A5}; A10 = ${A10}; A15 = ${A15}`)
+// console.log(A2)
+// console.log(A3)
+// console.log(A5)
+// console.log(A10)
+// console.log(A15)
